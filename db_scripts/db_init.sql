3ffd147686080629562e79f2644fff57a94b5554
Matias Netto <matiasnetto03@gmail.com>
2023-12-17 23:51:51 -0300

    Update db_init.sql added charlas entries

--- db_scripts/db_init.sql
+++ db_scripts/db_init.sql
@@ -24,14 +24,16 @@ CREATE TABLE oradores_topics (
     FOREIGN KEY (topic_id) REFERENCES topics(id)
 );
 
-CREATE TABLE charla (
+CREATE TABLE charlas (
     id int NOT NULL PRIMARY KEY  AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
+    name VARCHAR(255) NOT NULL,
     description VARCHAR(50) NOT NULL,
     start_time TIME NOT NULL,
     end_time TIME NOT NULL,
     topic_id int NOT NULL,
-    FOREIGN KEY (topic_id) REFERENCES topics(id)
+    orador_id int NOT NULL,
+    FOREIGN KEY (topic_id) REFERENCES topics(id),
+    FOREIGN KEY (orador_id) REFERENCES oradores(id)
 );
 
 
@@ -43,6 +45,10 @@ CREATE TABLE tikets (
     sex CHAR(1)
 );
 
+
+#################################
+#### INIT ORADORES Y TOPICOS ####
+#################################
 
 INSERT INTO topics (name, color_hex) VALUES
 ("JavaScript", "ffc107"),
@@ -63,3 +69,18 @@ INSERT INTO oradores_topics (orador_id, topic_id) VALUES
 (1, 1), (1,2), #steve:js - steve:react
 (2, 1), (2,2), #bill:js - bill:react
 (3,3), (3,4); #ada:negocios - ada:startup
+
+
+
+######################
+#### INIT CHARLAS ####
+######################
+
+
+INSERT INTO charlas (name, description, start_time, end_time, topic_id, orador_id)
+VALUES
+  ('Introducción a JavaScript', 'Explora los fundamentos de JavaScript, Descubre cómo JavaScript se ha convertido en la columna vertebral de la interactividad en la web dinámica.', '10:00:00', '11:00:00', 1, 1),
+  ('Desarrollo con React', 'Sumérgete en el emocionante mundo de React. Aprende las mejores prácticas, patrones y técnicas para desarrollar aplicaciones modernas y eficientes con la potencia de React.', '13:00:00', '13:30:00', 2, 2),
+  ('Estrategias de Negocios', 'Una charla de estrategias de negocios clave para el éxito empresarial. Exploraremos tácticas innovadoras, enfoques de mercado y decisiones estratégicas.', '15:00:00', '16:30:00', 3, 3),
+  ('Innovación en Startups', 'Descubre cómo la innovación impulsa el crecimiento en el ecosistema de startups. Examinaremos casos de estudio y prácticas de innovación de exitosas Startups.', '09:00:00', '10:30:00', 4, 1),
+  ('Avances en JavaScript', 'Mantente actualizado con los últimos avances en el mundo de JavaScript. Esta charla proporcionará una visión panorámica de los emocionantes desarrollos en el universo JavaScript.', '14:00:00', '15:30:00', 1, 2);
